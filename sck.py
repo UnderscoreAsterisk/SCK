ad092e7ff6484415d6925d89771180caf39e28ff
Apoorv Goel <apoorvgoel_bt2k16@dtu.ac.in>
2020-05-25 15:35:16 +0530

    Complete the remaining steps of the SCK algorithm

--- sck.py
+++ sck.py
@@ -1,9 +1,28 @@
 import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
 import spams
+import sys
 
 PATCH_SIZE=(11, 11)
+lower_CM = 7
+upper_CM = 150
+
+INPUT_FNAME = sys.argv[1]
+OUTPUT_FNAME = f"{INPUT_FNAME.split('.')[0]}_keypoints.png"
+
+class KeyPoint:
+    def __init__(self, X, Y, alpha):
+        self.X = X
+        self.Y = Y
+        self.alpha = alpha
+
+    @property
+    def SM(self):
+        return np.count_nonzero(self.alpha)+sum(abs(self.alpha))
+
+    @property
+    def CM(self):
+        return np.count_nonzero(self.alpha)
 
 def build_dict(x_len):
 
@@ -27,25 +46,58 @@ def build_dict(x_len):
     
     return np.hstack(waves)
 
-
+# Build Dictionary
 D = build_dict(PATCH_SIZE[0]*PATCH_SIZE[1])
-print(f"Generated Haar dictionary ({D.shape})")
+print(f"Generated Haar dictionary {D.shape}")
 D = np.asfortranarray(D)
 
-img = cv.imread('input_image.png')[:, :, 1]
-blur = cv.GaussianBlur(img, ksize=(5, 5), sigmaX=0)
+# Step1: Mean blur
+img = cv.imread(INPUT_FNAME)
+blur = cv.blur(img[:, :, 1], ksize=(11, 11))
 
-patches = []
+# Step2: blockwise sparse representation learning
+key_pts = []
 for i in range(0, blur.shape[0]-PATCH_SIZE[0], PATCH_SIZE[0]):
+    kp_row = []
     for j in range(0, blur.shape[1]-PATCH_SIZE[1], PATCH_SIZE[1]):
         patch = blur[i:i+PATCH_SIZE[0], j:j+PATCH_SIZE[1]].flatten()
         mu = patch.mean()
         sigma = patch.std()
-        X = np.asfortranarray((patch-mu)/sigma)
+        if sigma == 0:
+            X = np.asfortranarray(np.zeros(patch.shape))
+        else:
+            X = np.asfortranarray((patch-mu)/sigma)
         
-        alpha = spams.lasso(np.asfortranarray(X[:, None]), D, lambda1=0.25)
-        patches.append(alpha)
-        
+        alpha = spams.lasso(np.asfortranarray(X[:, None]), D, lambda1=0.125)
 
-patches = np.array(patches)
-print(patches.shape)
+        kp_row.append(KeyPoint(i+PATCH_SIZE[0]//2, j+PATCH_SIZE[1]//2, alpha.toarray()))
+    key_pts.append(kp_row)
+
+print(f"Keypoint matrix: ({len(key_pts)}, {len(key_pts[0])}) [Total = {len(key_pts)*len(key_pts[0])}]")
+
+max_y = len(key_pts)-1
+max_x = len(key_pts[0])-1
+suppresed_kp = []
+
+NEIGHBOURHOOD = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+# Step3: Non maxima suppression
+for y, row in enumerate(key_pts):
+    for x, kp in enumerate(row):
+        if y == 0 or y == max_y or x == 0 or x == max_x:
+            continue
+        elif kp.CM < lower_CM or kp.CM > upper_CM:
+            continue
+        else:
+            max_SM = kp.SM
+            for (dx, dy) in NEIGHBOURHOOD:
+                max_SM = max(max_SM, key_pts[y+dy][x+dx].SM)
+            if max_SM == kp.SM:
+                suppresed_kp.append(kp)
+
+print(f"{len(suppresed_kp)} Keypoints remain after suppression")
+
+#Draw the obtained keypoints on the image
+kp_size = int(np.sqrt((PATCH_SIZE[0]**2 + PATCH_SIZE[1]**2)/2))
+for kp in suppresed_kp:
+    cv.circle(img, (kp.Y, kp.X), kp_size//2, (0, 255, 0), 2)
+cv.imwrite(OUTPUT_FNAME, img)
